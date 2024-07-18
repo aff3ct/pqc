a2c5d799ecd260fa572f42074c497dda172cafac
alesavou <andrea.lesavourey@inria.fr>
2024-07-18 14:10:59 +0200

    comparator syndrome

---
+++ src/common/Modules/SyndComparator/SyndComparator.hpp
@@ -0,0 +1,32 @@
+#ifndef SYNDCOMPARATOR_H
+#define SYNDCOMPARATOR_H
+
+#include "streampu.hpp"
+
+namespace spu
+{
+    namespace module
+    {
+
+	class SyndComparator  : public Module {
+
+	private:
+    
+	    int frame_size;
+	    int synd_size;
+	    
+	public:
+
+	    SyndComparator(int frame_size, int synd_size);
+	    virtual ~SyndComparator();
+
+	protected:
+
+	    virtual void compare(int* input1, int* input2, int* input3, int *output,
+				 const int frame_id);
+
+	};
+    }
+}
+
+#endif // SYNDCOMPARATOR_H
